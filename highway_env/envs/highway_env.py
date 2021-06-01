061637f2ffcc627c10f2046be8aad5b5519c467d
Salihslh <81755767+Salihslh@users.noreply.github.com>
2021-06-01 12:18:17 +0200

    Update highway_env.py

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -75,14 +75,13 @@ class HighwayEnv(AbstractEnv):
                     other_vehicles_type.create_random(self.road, spacing=1 / self.config["vehicles_density"])
                 )
 
-    def _reward(self, action: int) -> float:
+    def _reward(self, action: Action) -> float:
         """
         The reward is defined to foster driving at high speed, on the rightmost lanes, and to avoid collisions.
         :param action: the last action performed
         :return: the corresponding reward
         """
         lane_change = action == 0 or action == 2
-        print(lane_change)
         neighbours = self.road.network.all_side_lanes(self.vehicle.lane_index)
         lane = self.vehicle.target_lane_index[2] if isinstance(self.vehicle, ControlledVehicle) \
             else self.vehicle.lane_index[2]
